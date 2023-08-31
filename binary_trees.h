f70e85b829e1bfb30b2039aae203ad6e924c7af5
R-oxy <aronmangati@gmail.com>
2023-08-30 23:50:44 -0700

    Adds a function that checks if a binary tree is a valid Binary Search Tree

--- binary_trees.h
+++ binary_trees.h
@@ -64,6 +64,5 @@ _Bool is_complete(const binary_tree_t *tree, unsigned int index, size_t size);
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
 int binary_tree_is_bst(const binary_tree_t *tree);
-int is_bst(const binary_tree_t *tree, const binary_tree_t **prev);
 
 #endif /* _BINARY_TREES_H_ */
